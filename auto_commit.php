5ce16d724b681a9293c846029bf5a25348389f1b
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-03 07:18:50 +0300

    Create auto_commit.php

---
+++ auto_commit.php
@@ -0,0 +1,24 @@
+<?php
+$mysqli = new mysqli("localhost","my_user","my_password","my_db");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+// Turn autocommit off
+$mysqli -> autocommit(FALSE);
+// Insert some values
+$mysqli -> query("INSERT INTO Persons (FirstName,LastName,Age)
+VALUES ('Peter','Griffin',35)");
+$mysqli -> query("INSERT INTO Persons (FirstName,LastName,Age)
+VALUES ('Glenn','Quagmire',33)");
+
+// Commit transaction
+if (!$mysqli -> commit()) {
+  echo "Commit transaction failed";
+  exit();
+}
+
+$mysqli -> close();
+?>
