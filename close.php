9c1386dc27b901d9ecd9f229444761ed9abe3684
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-04 14:30:00 +0300

    Create close.php

---
+++ close.php
@@ -0,0 +1,12 @@
+ <?php
+$mysqli = new mysqli("localhost","my_user","my_password","my_db");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+// ....some PHP code...
+
+$mysqli -> close();
+?> 
