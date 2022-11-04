79092a31106a577492b0ee94fea69e2ec21b7d30
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-04 14:53:57 +0300

    Create fetch_field.php

---
+++ fetch_field.php
@@ -0,0 +1,22 @@
+ <?php
+$mysqli = new mysqli("localhost","my_user","my_password","my_db");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+$sql = "SELECT Lastname, Age FROM Persons ORDER BY Lastname";
+
+if ($result = $mysqli -> query($sql)) {
+  // Get field information for all fields
+  while ($fieldinfo = $result -> fetch_field()) {
+    printf("Name: %s\n", $fieldinfo -> name);
+    printf("Table: %s\n", $fieldinfo -> table);
+    printf("Max. Len: %d\n", $fieldinfo -> max_length);
+  }
+  $result -> free_result();
+}
+
+$mysqli -> close();
+?> 
